cdc72f82a3d9764f61c2c564db9c000774860191
Howard Liberty <liberty@anymemo.org>
2019-04-15 13:52:53 -0700

    Main Activity rename

--- app/src/main/java/org/liberty/android/nc1020emu/MainActivity.java
+++ app/src/main/java/org/liberty/android/nc1020emu/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NC1020Activity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
