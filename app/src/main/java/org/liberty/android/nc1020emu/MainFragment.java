2a0f69138f9ecc1c022da15ca896a13837a29927
Howard Liberty <liberty@anymemo.org>
2019-04-15 13:51:27 -0700

    Apply new theme and move code to fragment

---
+++ app/src/main/java/org/liberty/android/nc1020emu/MainFragment.java
@@ -0,0 +1,295 @@
+package org.liberty.android.nc1020emu;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.view.Choreographer;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment implements SurfaceHolder.Callback, Choreographer.FrameCallback{
+    private static final int FRAME_RATE = 60;
+    private static final int FRAME_INTERVAL = 1000 / FRAME_RATE;
+    private static final long CYCLES_SECOND = 5120000;
+
+    private final byte[] lcdBuffer = new byte[1600];
+    private final byte[] lcdBufferEx = new byte[1600 * 8];
+
+    private Bitmap lcdBitmap;
+    private Matrix lcdMatrix;
+    private SurfaceHolder lcdSurfaceHolder;
+    private boolean speedUp;
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private boolean isRunning = true;
+
+    private float displayScale = 1f;
+    private long lastFrameTime;
+    private long lastCycles;
+    private long frames;
+    private TextView infoText;
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            long interval = 0L;
+            while (isRunning) {
+                long startTime = System.currentTimeMillis();
+                NC1020JNI.runTimeSlice((int)interval, speedUp);
+
+                if (NC1020JNI.copyLcdBuffer(lcdBuffer)) {
+                    synchronized (lcdBufferEx) {
+                        for (int y = 0; y < 80; y++) {
+                            for (int j = 0; j < 20; j++) {
+                                byte p = lcdBuffer[20 * y + j];
+                                for (int k = 0; k < 8; k++) {
+                                    lcdBufferEx[y * 160 + j * 8 + k] = (byte) ((p & (1 << (7 - k))) != 0 ? 0xFF
+                                            : 0x00);
+                                }
+                            }
+                        }
+                        for (int y = 0; y < 80; y++) {
+                            lcdBufferEx[y * 160] = 0;
+                        }
+                    }
+                }
+
+                long elapsed = System.currentTimeMillis() - startTime;
+                if (elapsed < FRAME_INTERVAL) {
+                    try {
+                        // If speedUp, don't sleep, run as much frames as system can
+                        Thread.sleep(speedUp ? 0 : FRAME_INTERVAL - elapsed);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                interval = Math.max(elapsed, FRAME_INTERVAL);
+            }
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return LayoutInflater.from(getContext()).inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        KeypadLayout keypad = view.findViewById(R.id.keypad_layout);
+        keypad.setOnButtonTouchListener(new KeypadLayout.OnButtonPressedListener() {
+            @Override
+            public void onKeyDown(int keyCode) {
+                NC1020JNI.setKey(keyCode, true);
+            }
+
+            @Override
+            public void onKeyUp(int keyCode) {
+                NC1020JNI.setKey(keyCode, false);
+            }
+        });
+
+        SurfaceView lcdSurfaceView = view.findViewById(R.id.lcd);
+        int width = getScreenWidth();
+        displayScale = (float) width / 160;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams( width, width / 2);
+        params.gravity = Gravity.CENTER_HORIZONTAL;
+        lcdSurfaceView.setLayoutParams(params);
+
+        lcdSurfaceHolder = lcdSurfaceView.getHolder();
+        lcdBitmap = Bitmap.createBitmap(160, 80, Bitmap.Config.ALPHA_8);
+        lcdMatrix = new Matrix();
+
+        infoText = view.findViewById(R.id.info);
+        lcdSurfaceHolder.addCallback(this);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        setupToolbar(toolbar);
+
+        String dir = initDataFolder(false);
+        NC1020JNI.initialize(dir);
+        NC1020JNI.load();
+    }
+
+    private void setupToolbar(Toolbar toolbar) {
+        //toolbar.setNavigationIcon(R.drawable.ic_launcher);
+        toolbar.inflateMenu(R.menu.main_menu);
+        toolbar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_quit:
+                    getActivity().finish();
+                    return true;
+                case R.id.action_restart:
+                    NC1020JNI.reset();
+                    return true;
+                case R.id.action_speed_up:
+                    if (item.isChecked()) {
+                        item.setChecked(false);
+                    } else {
+                        item.setChecked(true);
+                    }
+                    speedUp = item.isChecked();
+                    return true;
+                case R.id.action_load:
+                    NC1020JNI.load();
+                    return true;
+                case R.id.action_save:
+                    NC1020JNI.save();
+                    return true;
+                case R.id.action_factory_reset:
+                    showFactoryResetDialog();
+                    return true;
+
+                default:
+                    return false;
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Choreographer.getInstance().postFrameCallback(this);
+        isRunning = true;
+        executorService.submit(runnable);
+    }
+
+    @Override
+    public void onPause() {
+        isRunning = false;
+        Choreographer.getInstance().removeFrameCallback(this);
+        super.onPause();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        lcdMatrix.setScale(displayScale, displayScale);
+
+        Canvas lcdCanvas = lcdSurfaceHolder.lockCanvas();
+        lcdCanvas.drawColor(0xFF72B056);
+        lcdSurfaceHolder.unlockCanvasAndPost(lcdCanvas);
+    }
+
+
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+    private void showFactoryResetDialog() {
+        new AlertDialog.Builder(getContext())
+                .setTitle(R.string.factory_reset)
+                .setMessage(R.string.factory_reset_message)
+                .setPositiveButton(R.string.yes, (dialog, which) -> {
+                    isRunning = false;
+                    NC1020JNI.deleteStateAndNor();
+                    getActivity().recreate();
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+    }
+
+    private String initDataFolder(boolean override) {
+        File filesDir = getContext().getApplicationContext().getFilesDir();
+        try {
+            copyFileFromAsset("nc1020.fls", filesDir, override);
+            copyFileFromAsset("obj_lu.bin", filesDir, override);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return filesDir.getAbsolutePath();
+    }
+
+    private void copyFileFromAsset(String fileName, File folder, boolean override) throws IOException {
+        File dest = new File(folder.getAbsoluteFile() + "/" + fileName);
+        if (dest.exists() && !override) {
+            return;
+        }
+        try (InputStream in = getResources().getAssets().open(fileName)) {
+
+            try (FileOutputStream out = new FileOutputStream(dest)) {
+                byte[] buffer = new byte[8192];
+
+                int count;
+                while ((count = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, count);
+                }
+            }
+        }
+    }
+
+    private int getScreenWidth() {
+        Display display = getActivity().getWindowManager(). getDefaultDisplay();
+        Point size = new Point();
+        display. getSize(size);
+        return size.x;
+    }
+
+    private void updateLcd() {
+        Canvas lcdCanvas = lcdSurfaceHolder.lockCanvas();
+        if (lcdCanvas == null) {
+            return;
+        }
+
+        synchronized (lcdBufferEx) {
+            lcdBitmap.copyPixelsFromBuffer(ByteBuffer.wrap(lcdBufferEx));
+        }
+        lcdCanvas.drawColor(getResources().getColor(R.color.lcd_background));
+        lcdCanvas.drawBitmap(lcdBitmap, lcdMatrix, null);
+
+        lcdSurfaceHolder.unlockCanvasAndPost(lcdCanvas);
+    }
+
+    private void displayPerf() {
+        frames++;
+        long now = System.currentTimeMillis();
+        long elapse = now - lastFrameTime;
+        if (elapse > 1000L) {
+            long fps = frames * 1000 / elapse;
+            long cycles = NC1020JNI.getCycles();
+            long percentage = (cycles - lastCycles) * 100 / CYCLES_SECOND;
+            infoText.setText(String.format(getString(R.string.perf_text), fps, cycles, percentage));
+            lastCycles = cycles;
+            lastFrameTime = now;
+            frames = 0;
+        }
+
+    }
+
+    @Override
+    public void doFrame(long frameTimeNanos) {
+        displayPerf();
+        updateLcd();
+        Choreographer.getInstance().postFrameCallback(this);
+    }
+}
