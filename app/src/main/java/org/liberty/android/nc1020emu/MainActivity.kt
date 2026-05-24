45a479b204c61bec25e13f4f9fd3473fa9d7b7f6
Howard Liberty <liberty@anymemo.org>
2026-05-23 18:11:18 -0700

    Add light / dark theme selector

--- app/src/main/java/org/liberty/android/nc1020emu/MainActivity.kt
+++ app/src/main/java/org/liberty/android/nc1020emu/MainActivity.kt
@@ -2,10 +2,21 @@ package org.liberty.android.nc1020emu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.WindowCompat
+import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val themeValue = preferences.getString("theme", "system") ?: "system"
+        val mode = when (themeValue) {
+            "dark" -> AppCompatDelegate.MODE_NIGHT_YES
+            "light" -> AppCompatDelegate.MODE_NIGHT_NO
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        }
+        AppCompatDelegate.setDefaultNightMode(mode)
+
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.main_activity)
--- app/src/main/java/org/liberty/android/nc1020emu/SettingsFragment.kt
+++ app/src/main/java/org/liberty/android/nc1020emu/SettingsFragment.kt
@@ -2,13 +2,26 @@ package org.liberty.android.nc1020emu
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
+        findPreference<ListPreference>("theme")?.setOnPreferenceChangeListener { _, newValue ->
+            val themeValue = newValue as String
+            val mode = when (themeValue) {
+                "dark" -> AppCompatDelegate.MODE_NIGHT_YES
+                "light" -> AppCompatDelegate.MODE_NIGHT_NO
+                else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            }
+            AppCompatDelegate.setDefaultNightMode(mode)
+            true
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
